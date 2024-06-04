2235d2ea6f38167652740ef3bb3fb86a319b9a74
Mftr96 <matteo.federici96@gmail.com>
2024-06-04 10:51:27 +0200

    square right, aggiunta strong in OurWorks

--- src/components/AppMain/OurResults.vue
+++ src/components/AppMain/OurResults.vue
@@ -14,10 +14,61 @@ export default {
 }
 
 </script>
+
 <!-- QUI HTML -->
-<template>
+<template>  
+    <section class="our-results">
+        <div class="row">
+            <div class="col-6 square-left" >
+
+            </div>
+            <div class="col-6 p-3 square-right">
+                <div class="d-flex flex-column align-items-center">
+                    <h2 class="">We <strong>Love</strong> What We Do</h2>
+                    <div class="d-flex ">
+                        <div class="me-4"><hr class="line-short"></div>
+                        <div class=""><hr class="line-long"></div>
+                    </div>
+                    <p class="text-center">Far far away, behind the word mountains, far from the countries
+                        Vokalia and Conscnantia, there live the blind texts. Separated they
+                        live in Bookmarksgrove right at the coast of the Semantics, a large
+                        language ocean.Separated they live in Bookmarksgrove. Far far
+                        away, behind the word mountains. large language ocean. far from
+                        the countries Vokalia and Consonantia
+                    </p>
+                    <ul>
+                        <li><i class="fa-solid fa-check me-2"></i>far far away, behind the word mountains</li>
+                        <li><i class="fa-solid fa-check me-2"></i>large language ocean</li>
+                        <li><i class="fa-solid fa-check me-2"></i>far fram the countries Vakalia and Consanantia</li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    
 </template>
+
 <!-- QUI CSS -->
 <style scoped>
+.square-left{
+    border: 1px solid crimson;
+}
+.square-right{
+    border: 1px solid yellowgreen;
+}
+.line-short{
+    width: 20px;
+    color: green;
+}
 
+.line-long{
+    width: 60px;
+    color:green;
+}
+
+li{
+    list-style: none;
+}
 </style>
--- src/components/AppMain/OurWorks.vue
+++ src/components/AppMain/OurWorks.vue
@@ -17,11 +17,72 @@ export default {
 
 <!-- QUI HTML -->
 <template>
+    <section>
+        <div class="row debug-row" >
+            <div class="col-5 debug">
+                <h3> Our recent <strong>web designs</strong> & some examples of past <strong>projects</strong></h3>
+            </div>
+            <div class="col-7 debug">
+                <p>Far Far away, behind the word mountains, Far From the countries Vokalie end Consonentia, there live the
+                blind texts. Seperated they live in Bookmarksgrove righi the coesi: OF the Semantics, è large languege
+                ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountèins, far From the
+                countries Vokalia and Consonentia, there live the blind texts. Separated they live in Bookmarksgrave.</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row">
+            <div class="card border-0 gx-1  col-3">
+                <img src="../../assets\photo\images\ina-soulis-227104-unsplash-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets\photo\images\sunisa-misa-531163-unsplash-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets\photo\images\355H-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets\photo\images\photo-1448932252197-d19750584e56-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets\photo\images\business-competition-PB366D8-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets/photo/images/cozy-sofa-in-living-room-PQR5AB9-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets\photo\images\aa9a4539-PQGJ7HU-1024x1024.jpg" alt="">
+            </div>
+            <div class="card border-0 gx-1 col-3">
+                <img src="../../assets\photo\images\cody-davis-253928-unsplash-1024x1024.jpg" alt="">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row p-5 m-5" >
+                <div class="col-12 m-3 text-center">
+                <button><span>Read More</span></button>
+
+            </div>
+        </div>
+    </section>
  
 </template>
 
 
 <!-- QUI CSS -->
 <style scoped>
+.debug{
+    border: 1px dashed crimson;
+}
+.debug-row{
+    border: 1px solid black;
+}
+button{
+    background-color: rgb(134, 206, 117);  
+}
+span{
+    color: white;
+}
 
 </style>
