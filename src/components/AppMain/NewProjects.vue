1b41c99401a6ee91c912b4802f69e8e3b2aa8f25
Mftr96 <matteo.federici96@gmail.com>
2024-06-04 16:39:55 +0200

    newProjects e OurWorks

--- src/components/AppMain/NewProjects.vue
+++ src/components/AppMain/NewProjects.vue
@@ -6,22 +6,25 @@
 <!-- QUI HTML -->
 <template>
     <section id="NewProject">
-        <div class="row">
-            <div class="col-6 border text-center square-left">
-                <p>are you ready?</p>
-                <h3 class="text-light">Start a new project</h3>
-                <div class="input-box badge p-3 rounded-pill w-50">
-                    <input class="me-2 p-2" type="text" placeholder="Enter Your Email Adress">
+        <div class="row justify-content-center gap-4">
+            <div class="col-5 border text-center rounded-4  square-left">
+                <p class="mt-5">are you ready?</p>
+                <h3 class="text-light mb-5">Start a new project</h3>
+                <div class="input-box badge mb-5 p-3 rounded-pill w-50">
+                    <input class="me-2 border-0  p-2" type="text" placeholder="Enter Your Email Adress">
                     <a class="text-decoration-none text-secondary" href="">SUBMIT</a>
                 </div>
             </div>
-            <div class="col-6  text-center square-right">
-                <p>What are you Waiting For?</p>
-                <h3 class="text-light">Let's Talk About Work</h3>
-                <div class="">
+            <div class="col-5  text-center rounded-4 square-right">
+                <p class="mt-5">What are you Waiting For?</p>
+                <h3 class="text-light mb-5">Let's Talk About Work</h3>
+                <div class="mb-5">
                     <a href="">
                         <!-- chiedere bottone a Fabio  -->
-                        <button type="">START NOW</button>
+                        <button class="btn rounded-5 border-0 text-white px-5 py-3 shadow-lg mt-4 fs-6">
+                        <div class="background"></div>
+                        READ MORE
+                        </button>
                     </a>
                 </div>
 
@@ -34,14 +37,18 @@
 <!-- QUI CSS -->
 <style scoped>
 .square-left{
-    background-color: rgb(60, 60, 60);
+    background: linear-gradient(
+        271deg,
+        rgba(65, 65, 65, 1) 0%,
+        rgba(47, 47, 47, 1) 50%,
+        rgba(18, 20, 19, 1) 100%
+    );
 }
 
 .square-left p{
     color: rgb(153, 153, 153);
 }
 .input-box{
-    border: 1px solid crimson;
     background-color: rgb(62, 62, 62);
 }
 
@@ -50,11 +57,60 @@ input{
 }
 
 .square-right{
-    background-color: rgb(41, 219, 147);
-}
+    background: linear-gradient(
+        271deg,
+        rgba(122, 222, 108, 1) 0%,
+        rgba(88, 221, 125, 1) 60%,
+        rgba(0, 217, 166, 1) 100%
+    );  }
 
 .square-right p{
     color: rgb(204, 246, 222);
+}
+
+button {
+    font-size: 0.6rem;
+    display: inline-block;
+    position: relative;
+    background-color: #d5c264;
+    transition: color 0.5s;
+    z-index: 1;
+    overflow: hidden;
+}
+
+.background {
+    width: 0;
+    height: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    transition: 0.5s;
+    z-index: -1;
+    border-radius: 50%;
+    background: linear-gradient(
+        271deg,
+        rgba(122, 222, 108, 1) 0%,
+        rgba(88, 221, 125, 1) 60%,
+        rgba(0, 217, 166, 1) 100%
+    );
+}
+
+button:hover .background {
+    width: 150%;
+    height: 400%;
+}
+hr,
+.separator,
+button,
+.bgImg {
+    
+    background: linear-gradient(
+        271deg,
+        rgba(65, 65, 65, 1) 0%,
+        rgba(47, 47, 47, 1) 50%,
+        rgba(18, 20, 19, 1) 100%
+    );
 }
 
 </style>
