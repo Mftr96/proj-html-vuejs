5f8d18612248c96234069fd98215fcba8262a062
Mftr96 <matteo.federici96@gmail.com>
2024-06-05 16:18:20 +0200

    commit post-ticket

---
+++ src/components/AppHeader/CaroselHeader.vue
@@ -0,0 +1,142 @@
+<script>
+
+export default {
+    name: 'Carousel',
+    data() {
+        return {
+            slides: [
+                {
+                    image: 'Group-36-2x.png',
+                    title: 'Web Are a Web Design Agency',
+
+                },
+                {
+                    image: 'Group-35-2x.png',
+                    title: 'Focus on Your Business',
+
+                },
+                {
+                    image: 'Group-40-2x.png',
+                    title: 'A Group of Expert Planners',
+
+                }
+            ]
+        }
+    },
+
+    computed: {
+        carouselStyle() {
+            return {
+                transform: `translateX(-${this.currentIndex * 100}%)`,
+            };
+        },
+    },
+    methods: {
+        nextSlide() {
+            this.currentIndex =
+                (this.currentIndex + 1) % this.slides.length;
+        },
+        prevSlide() {
+            this.currentIndex =
+                (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+        },
+
+        getImg(path) {
+            let risultato = new URL("../../assets/photo/" + path, import.meta.url);
+            return risultato.href;
+        },
+    },
+    
+};
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<template>
+  <div class="carousel-container">
+        <button class="carousel-button left" @click="prevSlide">&#10094;</button>
+        <div class="carousel">
+            <div class="carousel-slides" :style="carouselStyle">
+                <div class="carousel-slide" v-for="slide in slides">
+                    <img :src="getImg (slide.image)" :alt="slide.text" class="carousel-image" />
+                    <div class="carousel-text">{{ slide.title }}</div>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-button right" @click="nextSlide">&#10095;</button>
+    </div>
+
+
+
+
+</template>
+
+
+
+<style scoped>
+.carousel-container {
+  position: relative;
+  width: 300px;
+  margin: 0 auto;
+}
+
+.carousel {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
+
+.carousel-slides {
+  display: flex;
+  transition: transform 0.5s ease-in-out;
+}
+
+.carousel-slide {
+  min-width: 100%;
+  box-sizing: border-box;
+}
+
+.carousel-image {
+  width: 100%;
+  height: auto;
+}
+
+.carousel-text {
+  text-align: center;
+  margin-top: 8px;
+}
+
+.carousel-button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0.5);
+  border: none;
+  color: white;
+  font-size: 18px;
+  padding: 8px;
+  cursor: pointer;
+  z-index: 10;
+}
+
+.carousel-button.left {
+  left: 0;
+}
+
+.carousel-button.right {
+  right: 0;
+}
+</style>
--- src/components/AppMain/LatestNews.vue
+++ src/components/AppMain/LatestNews.vue
@@ -35,9 +35,9 @@ export default{
             </div>
        </div>
        <div class="row">
-            <div class="col-4 rounded border border-1 border-black">
+            <div class="photo-box col-4 overflow-hidden rounded border border-1 border-black">
                 <img class=" w-100" src="../../assets\photo\images\serious-businesswoman-with-documents-talking-on-P9Q6LX6-1024.jpg" alt="">
-                <div class="info z-1 rounded bg-white   w-75 my-0 mx-auto">
+                <div class="info p-2 z-1 rounded bg-white   w-75 my-0 mx-auto">
                    <p>May 24.2018<i class="fa-solid fa-circle"></i> Alex</p>
                    <ul>
                     <li>Marketing Ideas</li>
@@ -59,6 +59,24 @@ export default{
 </template>
 
 <style scoped>
+.photo-box:hover img{
+    scale: 1.2;
+    transition: 0.5s;
+}
+.photo-box:hover .info{
+    transition: 0.5s;
+    scale: 1.1;
+    color: white;
+}
+.photo-box:hover .info{
+    background: linear-gradient(
+      271deg,
+      rgba(122, 222, 108, 1) 0%,
+      rgba(88, 221, 125, 1) 60%,
+      rgba(0, 217, 166, 1) 100%
+  );
+  transition: 0.5s;
+}
 .info{
     position: relative;
     bottom: 40px;
@@ -67,14 +85,14 @@ export default{
 .info:hover{
     /* gradiente preso dallo style generale */
     transition: 0.5s;
-    animation: mymove 5s ease ;
-    @keyframes mymove {
-        from{top:0px;}
-        to{top:20px;}
-        
-    }
+    scale: 1.1;
 }
 
+
+img:hover{
+    scale: 1.2;
+    transition: 0.5s;
+}
 ::marker{
     color: green;
     font-size: 25px;
